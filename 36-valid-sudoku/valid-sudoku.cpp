0b9056384f903f9a7616df82702f488f929f6d1e
shilpi kumari <134808437+shilpiig@users.noreply.github.com>
2026-04-28 22:44:14 +0530

    Time: 0 ms (100.00%) | Memory: 25.4 MB (24.64%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> rows(9), cols(9), boxes(9);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char val = board[i][j];
+                
+                if (val == '.') continue;
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                // Check if already exists
+                if (rows[i].count(val) || 
+                    cols[j].count(val) || 
+                    boxes[boxIndex].count(val)) {
+                    return false;
+                }
+
+                // Insert into sets
+                rows[i].insert(val);
+                cols[j].insert(val);
+                boxes[boxIndex].insert(val);
+            }
+        }
+
+        return true;
+    }
+};
