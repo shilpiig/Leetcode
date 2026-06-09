03f0f83f89eea0679cdd3cb90c2751b44e080ec4
shilpi kumari <134808437+shilpiig@users.noreply.github.com>
2026-06-09 21:54:49 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (10.29%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -18,3 +18,21 @@ public:
         return maxArea;        
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxArea(vector<int>& heights) {
+//         int n = heights.size();
+//         int maxArea = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i + 1; j < n; j++) {
+//                 int area = min(heights[i], heights[j]) * (j - i);
+//                 maxArea = max(maxArea, area);
+//             }
+//         }
+
+//         return maxArea;
+//     }
+// };
